eedbc0e19d9ed39e48b240b84df92d10eb96adfb
JesusLunaG <jlunag1501@alumno.ipn.mx>
2023-04-09 23:12:59 -0600

    Commit de prueba con dos mensajes

---
+++ Operadores/src/Operadores.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+        System.out.println("Esto es un mensaje"); //Mnesaje a traves de consola
+        JOptionPane.showMessageDialog(null,"Esto es un mensaje"); //Mensaje a traves de ventana
+    }
+}
